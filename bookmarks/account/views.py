3aad125b4d3d042605e3eafe73a16304938c8bb2
evgeny <zhenya.leshhinskij@gmail.com>
2023-12-12 17:48:02 +0300

    follow/unfollow

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,12 +1,15 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_POST
+from django.shortcuts import render, get_object_or_404
 from .forms import LoginForm, UserRegistrationForm, \
                    UserEditForm, ProfileEditForm
-from .models import Profile
+from .models import Profile, Contact
+from actions.utils import create_action
+from actions.models import Action
 
 
 def user_login(request):
@@ -32,24 +35,35 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
+    # По умолчанию показать все действия
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    if following_ids:
+        # Если пользователь подписан на других,
+        # то извлечь только их действия
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile')[:10]
     return render(request,
                   'account/dashboard.html',
-                  {'section': 'dashboard'})
+                  {'section': 'dashboard',
+                   'actions': actions})
 
 
 def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            # Create a new user object but avoid saving it yet
+            # Создать новый объект пользователя,
+            # но пока не сохранять его
             new_user = user_form.save(commit=False)
-            # Set the chosen password
+            # Установить выбранный пароль
             new_user.set_password(
                 user_form.cleaned_data['password'])
-            # Save the User object
+            # Сохранить объект User
             new_user.save()
-            # Create the user profile
+            # Создать профиль пользователя
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -104,3 +118,27 @@ def user_detail(request, username):
                   'account/user/detail.html',
                   {'section': 'people',
                    'user': user})
+
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user)
+                create_action(request.user, 'is following', user)
+            else:
+                Contact.objects.filter(user_from=request.user,
+                                       user_to=user).delete()
+            return JsonResponse({'status':'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status':'error'})
+    return JsonResponse({'status':'error'})
+
+
